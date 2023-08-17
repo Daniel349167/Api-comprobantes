20984b655858ee2b94bfaa29592e47edf871f82a
Daniel Ureta <duretae@gmail.com>
2023-08-17 07:37:12 -0500

    Apis casi listas

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ComprobanteController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Autenticación
+Route::post('/users', [UserController::class,'store']);  //Solo por motivos de pruebas se da esta api sin middelware
+Route::post('/login', [AuthController::class, 'login']); // Login y obtención del JWT
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('jwt.verify');; // Logout y invalidación del JWT
+
+// Grupo de rutas protegidas con el middleware JWT para asegurarse de que solo los usuarios autenticados puedan acceder.
+Route::group(['middleware' => ['jwt.verify']], function () {
+
+    // Comprobantes
+    Route::post('/comprobantes', [ComprobanteController::class, 'store']); // Registrar uno o varios comprobantes
+    Route::get('/comprobantes', [ComprobanteController::class, 'index']); // Listar comprobantes de forma paginada
+    Route::get('/comprobantes/{id}', [ComprobanteController::class, 'show']); // Obtener detalles de un comprobante específico
+    Route::delete('/comprobantes/{id}', [ComprobanteController::class, 'destroy']); // Eliminar un comprobante
+
+    Route::get('/user/{userId}/articulo/{nombreArticulo}/monto-total', [ComprobanteController::class, 'getTotalAmountByArticle']); // Monto total por artículo de un usuario en específico  
+    Route::get('/user/{userId}/comprobantes/monto/total', [ComprobanteController::class, 'getTotalAmount']); // Monto total acumulado por todos los comprobantes de un usuario específico
+    
+
 });
